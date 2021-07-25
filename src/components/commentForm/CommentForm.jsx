a2433f0d2c7edf357a22da3292e26cbea02fa314
jennifer astocondor <jennifer031907@yahoo.com>
2021-07-24 20:36:43 -0400

    comment form

--- src/components/commentForm/CommentForm.jsx
+++ src/components/commentForm/CommentForm.jsx
@@ -1,21 +1,52 @@
-import React from 'react';
-import UserPic from '../../assets/images/Mohan-muruge.jpg'
-import './CommentForm.scss'
+import React from "react";
+import UserPic from "../../assets/images/Mohan-muruge.jpg";
+import "./CommentForm.scss";
 
-const CommentForm = () => {
-  return (
-    <div className="CommentForm">
-      <img className="CommentForm--pic" src={UserPic} alt="User Pic"/>
-      <div>
-        <div className="CommentForm__form">
-          <label className="CommentForm__form--label">JOIN THE CONVERSATION</label>
-          <textarea className="CommentForm__form--input" type="text" placeholder="Write comment here" />
-        </div>
+class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        value: ''
+    };
+  } 
+
+  handleOnChange = (e) => {
+    this.setState({ value: e.target.value})
+  }
+
+  handleOnClick = () => {
+    this.props.handleOnClick(this.state.value);
+    this.setState({ value: '' });
+  }
+  render() {
+    return (
+      <div className="CommentForm">
+        <img className="CommentForm--pic" src={UserPic} alt="User Pic" />
         <div>
-          <button className="CommentForm__form--button" onClick={() => {}}>COMMENT</button></div>
+          <div className="CommentForm__form">
+            <label className="CommentForm__form--label">
+              JOIN THE CONVERSATION
+            </label>
+            <textarea
+              className="CommentForm__form--input"
+              type="text"
+              placeholder="Write comment here"
+              value={this.state.value}
+              onChange={this.handleOnChange}
+            />
+          </div>
+          <div>
+            <button
+              className="CommentForm__form--button"
+              onClick={this.handleOnClick}
+            >
+              COMMENT
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-  )
-}
+    );
+ }
+};
 
 export default CommentForm;
