b5c6d1320318429903a2c6adad41b1caeb8fbe3d
jennifer astocondor <jennifer031907@yahoo.com>
2021-07-25 21:27:50 -0400

    page header upload page

--- src/components/pageHeader/PageHeader.jsx
+++ src/components/pageHeader/PageHeader.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import logo from '../../assets/logo/Logo-brainflix.svg'
-import UserPic from '../../assets/images/Mohan-muruge.jpg'
-import './PageHeader.scss'
+import React from "react";
+import logo from "../../assets/logo/Logo-brainflix.svg";
+import UserPic from "../../assets/images/Mohan-muruge.jpg";
+import { Link } from "react-router-dom";
+import "./PageHeader.scss";
 
 function PageHeader() {
   return (
-    <header className="header" >
-      <div className="header--logo">
-        <img src={logo} alt="logo graphic" />
-      </div>
+    <header className="header">
+      <Link to="/home">
+        <div className="header--logo">
+          <img src={logo} alt="logo graphic" />
+        </div>
+      </Link>
       <div className="header__search">
         <input className="header__search--input" placeholder="Search" />
-        <div>
-          <button className="header__search--button" onClick={() => {}}>+ UPLOAD</button>
-          <img className="header__search--image" src={UserPic} alt="User Pic"/>
-        </div>
+        <Link to="/upload" className="text-decoration-none">
+          <button className="header__search--button">+ UPLOAD</button>
+        </Link>
+
+        <img className="header__search--image" src={UserPic} alt="User Pic" />
       </div>
     </header>
-  )
+  );
 }
 export default PageHeader;
