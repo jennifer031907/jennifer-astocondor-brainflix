6114c0d16e6a0fead1adf33bcf3abf6ff57669ba
jennifer astocondor <jennifer031907@yahoo.com>
2021-08-01 07:17:13 -0400

    remove import  pageheader

--- client/src/pages/VideoUploadPage.jsx
+++ client/src/pages/VideoUploadPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import PageHeader from '../components/pageHeader/PageHeader'
 import VideoUpload from '../components/videoUpload/VideoUpload'
 
 class VideoUploadPage extends React.Component {
@@ -9,7 +8,6 @@ class VideoUploadPage extends React.Component {
 
     return (
       <>
-        <PageHeader />
         <VideoUpload />
       </>
     );
