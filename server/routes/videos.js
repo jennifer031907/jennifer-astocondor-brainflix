99d12a061c14372deb9a26563ad0a767b355fd09
jennifer astocondor <jennifer031907@yahoo.com>
2021-08-01 22:24:55 -0400

    final commit

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const fs = require('fs');
 const db = './data/videos.json'
-const videos = JSON.parse(fs.readFileSync(db, 'utf8'));
+let videos = JSON.parse(fs.readFileSync(db, 'utf8'));
 
 router.get('/', (req, res) => {
     const formattedVideos = videos
@@ -29,8 +29,25 @@ router.post('/', (req, res) => {
     const video = req.body;
     videos.push(video);
     const videosStringify = JSON.stringify(videos);
-    fs.writeFileSync(db,videosStringify);
+    fs.writeFileSync(db, videosStringify);
     res.json(video);
+});
+router.post('/:id/comments', (req, res) => {
+    const { id } = req.params;
+    let videoFound = null;
+    const comment = req.body;
+    videos = videos.map(video => {
+        if (video.id === id) {
+            videoFound = video;
+            video.comments.push(comment);
+        }
+        return video;
+    })
+    if (videoFound) {
+        res.json(comment);
+    } else {
+        res.status(404).json(generageError('No video with that id exists'));
+    }
 });
 const generageError = error => {
     return { message: error }
